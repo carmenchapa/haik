5f87596541557773aa2674e268483e4282842555
Carmen Chapa <cmchapa28@gmail.com>
2017-05-03 16:36:09 +0100

    change target to cta and getting tired

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 
-var advert, preloader, progress, progressBar, counter;
+var advert, preloader, progress, progressBar, counter, progressFill, cta, haiker;
 
 window.onload = function() {
 	canvas = document.getElementById('main');
 	canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
+    $('.cta').css({'display':'none'});
+    $('.progress').css({'display':'none'});
 
+  //prevent scrolling (onmousewheel for Firefox)
+	document.getElementById( "main" ).onwheel = function(event){
+    event.preventDefault();
+	};
+	document.getElementById( "main" ).onmousewheel = function(event){
+	    event.preventDefault();
+	};
+	$("#main").bind('mousedown touchstart mousemove touchmove', function(e){
+		e.preventDefault();
+	})
 	setElements();
 	adVisibilityHandler();	
 };
@@ -42,9 +54,7 @@ function enablerInitHandler() {
 function setElements(){
 		$('#container').css({"width":canvas.width,"height":canvas.height})
 		$('.preloader').delay(1250).fadeOut('slow');
-		$('.preloader-wrapper').delay(1250);
-		$('.progress').css({'top':canvas.height});
-		
+		$('.preloader-wrapper').delay(1250);		
 }
 
 function adVisibilityHandler() {
@@ -52,11 +62,12 @@ function adVisibilityHandler() {
 	var queue = new createjs.LoadQueue(true);
 	
 	queue.on('complete', handleAllImagesLoaded, this);
-	
+
 	// queue.loadFile({id:"cta", src: dynamicContent.Feed_300x600[0].content['cta.png'].Url });
 
 	queue.loadFile({id:"cta", src: "assets/cta.png"});
-	
+
+	// handleAllImagesLoaded();
 	function handleAllImagesLoaded() {
 		console.log('All images loaded');
 
@@ -71,7 +82,6 @@ function adVisibilityHandler() {
 // function bgExitHandler() {
 //     var exitId = 'Primary Exit';
 //     // var dynamicExitUrl = dynamicContent.Feed_300x600[0].ClickTag.Url;
-
 //     if (dynamicExitUrl) {
 //         Enabler.exitOverride(exitId, dynamicExitUrl);
 //     } else {
@@ -92,6 +102,7 @@ console.log('assets');
 function Main(assets) {
 console.log('main');
 	var stage = new createjs.Stage("main");
+	var haiker = new Haiker();
 	var progress = new ProgressBar();
 	var legals = new SkyLegals();
 	
@@ -131,54 +142,62 @@ console.log('main');
 	}
 }
 
+function Haiker(){
+	haiker = document.getElementById('cta');
+	setTimeout(function(){
+		haiker.style.top = window.innerHeight - 120 + 'px';
+		haiker.style.left = window.innerWidth / 2 + 'px';		
+		$(haiker).css({'display':'block'});
+	},500);
+}
+
 function ProgressBar(){
 	console.log('progress');
 	progress = document.getElementById('progress');
 	progressBar = document.getElementById('determinate');
 	setTimeout(function(){
-		progress.style.top = '70px';
-		
-	},1000)
-	
+		progress.style.top = '60px';
+		$(progress).css({'display':'block'});		
+	},1000);
 }
 
 function Frame01(stage, assets) {
-	console.log('f1');
-
-	var cta = new createjs.Bitmap(assets.cta.img);
-	$(cta).css("z-index", "2");
-	cta.width = 100;
-	cta.height = 203;
-	cta.x = window.innerWidth / 2;
-	cta.y = window.innerHeight - 120;
-	cta.regX = cta.width / 2;
-	cta.regY = cta.height / 2;
-	cta.alpha = 0;
-	
-	
 
 	this.animate = function(animationComplete) { 
-		stage.addChild(cta);
+		// stage.addChild(cta);
 		
 		// createjs.Tween.get(cta).to({alpha: 1},500).wait(300).to({ y: cta.y - 20 },250).to({y: cta.y},250);;
-		createjs.Tween.get(cta).to({alpha: 1},400).wait(300).to({ y: cta.y - 50 },250, createjs.Ease.circIn).to({y: cta.y},250, createjs.Ease.circOut);
+		// createjs.Tween.get(cta).to({alpha: 1},400).wait(300).to({ y: cta.y - 50 },250, createjs.Ease.circIn).to({y: cta.y},250, createjs.Ease.circOut);
+		// createjs.Tween.get(haiker).to({alpha: 1},400).wait(300).to({ y: haiker.style.top - 50 },250, createjs.Ease.circIn).to({y: haiker.style.top},250, createjs.Ease.circOut);
+		// TweenMax.to(document.getElementById('cta'), 0.2, { y: 10, delay: 0.5 });
 
 		
 		TweenLite.delayedCall(26, function(){
 			animationComplete();
 		});
+
 	};
 
 	//moof thing
-	// var firstPosX = cta.x;
-	// var firstPosY = cta.y;
+
+		// cta.on("pressmove", function(evt) {
+		//     evt.target.x = evt.stageX;
+		//     evt.target.y = evt.stageY;
+		// });
+		// cta.on("pressup", function(evt) { console.log("up"); })
+		cta = document.getElementById('cta');
 
 		var startX, startY;
 		counter = 0;
+		progressFill = 100;
 		//$('#main').mousedown(function(e){
-		$('#main').bind("mousedown touchstart", function(e){
-			console.log(e.type=='touchstart');
-			console.log(e.type=='mousedown');
+		// $('#main').bind("mousedown touchstart",function(e){
+		// cta.on("touchstart", function(e){
+
+		$(cta).bind("mousedown touchstart",function(e){
+
+			console.log('touchstart: ', e.type=='touchstart');
+			console.log('mousedown: ', e.type=='mousedown');
 			e.preventDefault();
 			if(e.type=='touchstart'){
 				var touch = e.originalEvent.touches[0];
@@ -191,36 +210,72 @@ function Frame01(stage, assets) {
 		  console.log('sart x: ' + startX + ' start y: ' + startY);  
 		})
 
-		//$('#main').mouseup(function(e){
-		$('#main').bind("mouseup touchend",function(e){
+		// $('#main').mouseup(function(e){
+		// $('#main').bind("mouseup touchend",function(e){
+		// cta.on("touchend",function(e){
+
+
+		// $(cta).bind("mouseup touchend",function(e){
+			// 	e.preventDefault();
+			// 	if(e.type=='touchend'){
+			// 		  var touch = e.originalEvent.changedTouches[0];
+			// 		  var endX = touch.clientX;
+			// 		  var endY = touch.clientY;
+			// 	}else if(e.type=='mouseup'){
+			//			var endX = e.clientX;
+			//			var endY = e.clientY;			
+			// 	}
+
+		var endX, endY;
+		$(cta).on("touchend",function(e){
 			e.preventDefault();
-			if(e.type=='touchend'){
-				  var touch = e.originalEvent.changedTouches[0];
-				  var endX = touch.clientX;
-				  var endY = touch.clientY;
-			}else if(e.type=='mouseup'){
-				  var endX = e.clientX;
-		  		  var endY = e.clientY;			
-			}
+			var touch = e.originalEvent.changedTouches[0];
+			 endX = touch.clientX;
+			 endY = touch.clientY;
+			move();
+		});
+		$('#main').mouseup(function(e){
+			e.preventDefault();
+			 endX = e.clientX;
+			 endY = e.clientY;	
+			move();
+		});
+
+
+		function move(){
 		  var deltaX = endX - startX;
 		  var deltaY = endY - startY;
 		  console.log('end x: ' + endX + ' end y: ' + endY);  
 		  console.log ("deltaX: " + deltaX + ' deltaY, ' + deltaY);
 		  
-    	  console.log("Top: " + cta.x + " Left: " + cta.y);
-
+    	  
+	//for some reason this is all wrong, in some directions is being tyred and in some others is just the oposite.!!!
 		  if(deltaY<0){
-		  	createjs.Tween.get(cta).wait(100).to({x:(deltaX + cta.x), y:(deltaY*0.5 + cta.y)},createjs.Ease.cubicInOut(5));
-		  	counter += 2;
+		  	// createjs.Tween.get(cta).wait(100).to({x:progressFill*0.01*(deltaX + cta.x), y:progressFill*0.01*(deltaY*0.5 + cta.y)},createjs.Ease.cubicInOut(5));
+
+		  	var offset = $('.cta').offset();		  	
+		  	TweenMax.to(cta, 0.2, {left:progressFill*0.01*(deltaX) + offset.left, top:progressFill*0.01*(deltaY*0.5) + offset.top, delay: 0.1});
+		  	var offset = $('.cta').offset();
+		  	console.log ("cta.x: " + offset.left + ' cta.y, ' + offset.top);
+		  	counter += 4;
 		  	console.log('counter: ' + counter);
-		  	progressBar.style.width = 100 - counter + '%';
+		  	console.log('progressFill: ' + progressFill);
+		  	progressFill = 100 - counter;
+		  	progressBar.style.width = progressFill + '%';
 		  }else{
-		  	createjs.Tween.get(cta).wait(100).to({x:(deltaX + cta.x), y:(deltaY*1.25 + cta.y)},createjs.Ease.cubicInOut(5));
-			counter += 1;
+		  	// createjs.Tween.get(cta).wait(100).to({x:progressFill*0.01*(deltaX + cta.x), y:progressFill*0.01*(deltaY*1.25 + cta.y)},createjs.Ease.cubicInOut(5));
+
+		  	var offset = $('.cta').offset();		  	
+		  	TweenMax.to(cta, 0.2, {left:progressFill*0.01*(deltaX) + offset.left, top:progressFill*0.01*(deltaY*1.25) + offset.top, delay: 0.1});
+		  	var offset = $('.cta').offset();
+		  	console.log ("cta.x: " + offset.left + ' cta.y, ' + offset.top);
+			counter += 2;
 		  	console.log('counter: ' + counter);
-		  	progressBar.style.width = 100 - counter + '%';
+		  	console.log('progressFill: ' + progressFill);
+		  	progressFill = 100 - counter;
+		  	progressBar.style.width = progressFill + '%';
 		  }
-		})
+		}
 }
 
 function SkyLegals() {
